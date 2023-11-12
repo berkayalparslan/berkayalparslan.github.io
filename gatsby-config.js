a5246685a588a731058c77f96b00629a44ccf6b5
Berkay Alparslan <15653983+berkayalparslan@users.noreply.github.com>
2023-11-12 19:30:58 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `berkayalparslan.github.io`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
