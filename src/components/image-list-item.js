7ff325f99011fd0a226a402f8b75d57df131b04a
Berkay Alparslan <15653983+berkayalparslan@users.noreply.github.com>
2023-11-23 00:58:38 +0100

    added hovering effect on tech and tools list

--- src/components/image-list-item.js
+++ src/components/image-list-item.js
@@ -1,11 +1,39 @@
 import * as React from "react";
-import {Image} from "react-bootstrap";
-import {imageListItem} from "./layout.module.css";
+import { useState } from "react";
+import { Image } from "react-bootstrap";
+import { imageListItem } from "./layout.module.css";
+
+const imageSize = "48vh";
 
 const ImageListItem = ({ imgSource, altText }) => {
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseIn = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
   return (
-    <div className={`${imageListItem}`}>
-      <Image className={`img-fluid`} src={imgSource} alt={altText}/>
+    <div
+      className={`${imageListItem}`}
+      onMouseOver={handleMouseIn}
+      onMouseOut={handleMouseOut}
+    >
+      <Image
+        width={imageSize}
+        height={imageSize}
+        src={imgSource}
+        alt={altText}
+      />
+      <p
+        className={`text-nowrap text-decoration-underline ${
+          !isHovering ? "invisible " : ""
+        }`}
+      >
+        {altText}
+      </p>
     </div>
   );
 };
