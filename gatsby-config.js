061d75e589e971b9aa5d3657b085946c79b4b640
Berkay Alparslan <15653983+berkayalparslan@users.noreply.github.com>
2023-11-27 21:55:49 +0100

    Revert "Updates"
    This reverts commit d09336fce5b0328744fb6e001091452a37efe391.

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,3 +9,15 @@ module.exports = {
   },
   plugins: [],
 }
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  pathPrefix: "/",
+  siteMetadata: {
+    title: `berkayalparslan.github.io`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
